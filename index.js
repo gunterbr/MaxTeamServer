5a3864bdcff75b942869f11184d94ddc80971bfd
gunterbr <josiel_jwk@hotmail.com>
2022-08-01 11:07:04 -0400

    change

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
-const express = require('express')
-const mysql = require('mysql')
-const bodyParser = require('body-parser')
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-const app = express()
+const PORT = process.env.PORT || 3050;
 
+const app = express();
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-});
-
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
 	host: process.env.MYSQL_HOST,
 	database: process.env.MYSQL_DATABASE,
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
 });
 
-app.post('/add', (req, res) => {
-	const sql = 'INSERT INTO login SET ?';
-  
-	const customerObj = {
-	  user: req.body.user,
-	  username: req.body.username,
-	  password: req.body.password
-	};
-  
-	db.query(sql, customerObj, error => {
-	  if (error) throw error;
-	  res.send('Customer created!');
-	});
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
 });
 
-app.listen(process.env.PORT || 3000, function(){
-	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.post('/add', (req, res) => {
+  const sql = 'INSERT INTO login SET ?';
+
+  const customerObj = {
+    user: req.body.user,
+    username: req.body.username,
+	password: req.body.password
+  };
+
+  connection.query(sql, customerObj, error => {
+    if (error) throw error;
+    res.send('Customer created!');
+  });
 });
+
+connection.connect(error => {
+  if (error) throw error;
+  console.log('Database server running!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
