ee947687770864d8a4ba6451c1ff9c4777d9461e
gunterbr <josiel_jwk@hotmail.com>
2022-08-01 10:59:55 -0400

    unico teste

--- index.js
+++ index.js
@@ -27,112 +27,18 @@ const db = mysql.createConnection({
 	password: process.env.MYSQL_PASSWORD,
 });
 
-//USUÁRIOS
-app.post('/api/login', (req, res) => {
-	const { username, password } = req.body;
-	
-	let mysql = `SELECT * FROM login WHERE username = ? AND password = ?`;
-	db.query(mysql, [username, password], (err, result) => {
-		if (err) {
-			res.status(500).send(err)
-		} else {
-			if (result.length > 0) {
-				res.status(200).send({
-					"id": result[0].id,
-					"user": result[0].user,
-					"username": result[0].username
-				})
-			} else {
-				res.status(400).send('Usuário ou senha incorretos');
-			}
-		}
-	});
-});
-
-app.post('/api/newuser', (req, res) => {
-	const user = req.body.user;
-	const username = req.body.username;
-	const password = req.body.password;
-
-	if(!user || !username || !password) {
-		res.status(400).send('Preencha todos os campos')
-	} else {
-	
-	let mysql = `INSERT INTO login (user, username, password) VALUES (?, ?, ?)`;
-	db.query(mysql, [user, username, password], (err, result) => {
-		if (err) {
-			res.status(500).send(err)
-		} else {
-			if (result.affectedRows > 0) {
-				res.status(200).send({
-					"user": result.user,
-					"username": result.username,
-					"password": result.password
-				})
-			} else {
-				res.status(400).send('Tente um username diferente')
-			}
-		}
-	});
-	}
-});
-
-//EVENTOS
-app.post("/register", (req, res) => {
-	const { username, title, start, end, backgroundColor } = req.body;
+app.post('/add', (req, res) => {
+	const sql = 'INSERT INTO login SET ?';
   
-	let mysql = `INSERT INTO events (username,backgroundColor, title, start, end) VALUES (?, ?, ?, ?, ?)`;
-	db.query(mysql, [username, backgroundColor, title, start, end], (err, result) => {
-		if(err) return console.log("Verifique as informações e tente novamente!")
-		res.send(result);
-	});
-});
+	const customerObj = {
+	  user: req.body.user,
+	  username: req.body.username,
+	  password: req.body.password
+	};
   
-app.get("/search", (req, res) => {
-	const { title, username } = req.body;
-
-	let mysql =
-	  `SELECT * FROM events WHERE title = ? AND username = ?`;
-	db.query(mysql, [title, username], (err, result) => {
-	  if (err) res.send(err);
-	  res.send(result);
-	});
-});
-  
-app.get("/getCards/:username", (req, res) => {
-	const { username } = req.params;
-
-	let mysql =
-	  `SELECT * FROM events WHERE username = ?`;
-	db.query(mysql, username, (err, result) => {
-	  if (err) res.send(err);
-	  res.send(result);
-	});
-});
-  
-app.put("/edit", (req, res) => {
-	const { id, title, start, end } = req.body;
-	
-	let mysql = `UPDATE events SET title = ?, start = ?, end = ? WHERE id = ?`;
-	db.query(mysql, [title, start, end, id], (err, result) => {
-	  if (err) {
-		res.send(err);
-	  } else {
-		res.send(result);
-	  }
-	});
-});
-  
-app.delete("/delete/:id", (req, res) => {
-	const { id } = req.params;
-	
-	let mysql = `DELETE FROM events WHERE id = ?`;
-	db.query(mysql, id, (err, result) => {
-	  if (err) {
-		console.log(err);
-	  } else {
-		res.send(result);
-	  }
+	db.query(sql, customerObj, error => {
+	  if (error) throw error;
+	  res.send('Customer created!');
 	});
 });
 
