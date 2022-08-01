cc78e68b21a5cc0843d93f3c14aea522690b0c24
gunterbr <josiel_jwk@hotmail.com>
2022-08-01 17:33:06 -0400

    commit

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
+const express = require('express')
+const mysql = require('mysql2')
+const bodyParser = require('body-parser')
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 
 const connection = mysql.createPool({
 	host: process.env.MYSQL_HOST,
 	database: process.env.MYSQL_DATABASE,
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
-});
+})
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my API!');
-});
+  res.send('Welcome to my API!')
+})
 
 app.post('/add', (req, res) => {
   
-  const {
-    user,
-    username,
-    password
-  } = req.body;
+  const { user, username, password } = req.body
 
-  const sql = `INSERT INTO login (user, username, password) VALUES (?, ?, ?)`;
+  const verificar_username = `SELECT username FROM login WHERE username = ${username}`
 
-  connection.query(sql, [user, username, password], error => {
-    if (error) throw error;
-    res.send('Customer created!');
-  });
-});
+  if(!verificar_username) {
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+    const sql = `INSERT INTO login (user, username, password) VALUES (?, ?, ?)`
+
+    connection.query(sql, [user, username, password], error => {
+      if (error) throw error
+      res.send('Usuário cadastrado com sucesso!')
+    })
+  
+  } else {
+    res.send('Este username já está sendo utilizado!')
+  }
+})
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
