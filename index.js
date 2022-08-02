f1574be3ef12236f66a052c541e9a78ce0145355
gunterbr <josiel_jwk@hotmail.com>
2022-08-02 10:00:35 -0400

    commit

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.post('/newuser', (req, res) => {
   const check = `SELECT COUNT(*) AS equalUser FROM login WHERE username = ?`
   connection.query(check, username, (err, count) => {
     const result = JSON.stringify(count[0].equalUser)
-    if (err) throw err
     if (result > 0) {
       res.send('Username indisponível!')
       process.exit(1)
@@ -32,9 +31,10 @@ app.post('/newuser', (req, res) => {
 
   const sql = `INSERT INTO login (user, username, password) VALUES (?, ?, ?)`
   connection.query(sql, [user, username, password], err => {
-    if (err) throw err
-    res.send('Usuário cadastrado com sucesso!')
-    process.exit(1)
+    if (!err) {
+      res.send('Usuário cadastrado com sucesso!')
+      process.exit(1)
+    }
   })
 
 })
