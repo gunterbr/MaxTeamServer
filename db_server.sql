90939b41587c60cb63d90408c6cf29d88da99cab
gunterbr <josiel_jwk@hotmail.com>
2022-08-02 13:07:25 -0400

    commit

--- db_server.sql
+++ db_server.sql
@@ -1,17 +1,20 @@
-CREATE TABLE `o8usy5kkwtym7eo6`.`events` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `backgroundColor` VARCHAR(45) NULL,
-  `title` VARCHAR(45) NOT NULL,
-  `start` DATETIME NOT NULL,
-  `end` DATETIME NULL,
+CREATE TABLE `inscricao` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomeCandidato` varchar(45) NOT NULL,
+  `evento` varchar(45) NOT NULL,
+  `pagamento` blob NOT NULL,
+  `numeroInscricao` int NOT NULL,
+  `deferida` varchar(45) DEFAULT NULL,
+  `responsavel` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `title_UNIQUE` (`title` ASC) VISIBLE);
+  UNIQUE KEY `numeroInscricao_UNIQUE` (`numeroInscricao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `o8usy5kkwtym7eo6`.`login` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user` VARCHAR(45) NOT NULL,
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`, `username`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);
+CREATE TABLE `login` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`,`username`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- index.js
+++ index.js
@@ -7,18 +7,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
       'Access-Control-Allow-Header',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
+  )
 
   if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).send({});
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+      return res.status(200).send({})
   }
-  next();
-});
+  next()
+})
 
 const connection = mysql.createPool({
 	host: process.env.MYSQL_HOST,
@@ -31,6 +31,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to my API!')
 })
 
+//Novo Usuário. Use o Insomnia e o modelo userAdmin.json
 app.post('/newuser', (req, res) => {
 
   const { user, username, password } = req.body
@@ -50,6 +51,38 @@ app.post('/newuser', (req, res) => {
       })
     }
   })
+
+})
+
+//Login
+app.get('/login', (req, res) => {
+
+
+
+})
+
+//Inscrição
+app.post('/inscricao', (req, res) => {
+
+  const { candidato, evento, pagamento } = req.body
+
+  const query = 'INSERT INTO inscricao SET ?',
+        values = {
+          nomeCandidato: candidato,
+          evento: evento,
+          pagamento: pagamento
+        }
+  connection.query(query, values, err => {
+    if (err) throw err
+    res.send('Inscrição realizada!')
+  })
+
+})
+
+//Confirmar Inscrição
+app.put('/confirmar', (req, res) => {
+
+  
 
 })
 
