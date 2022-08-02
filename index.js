714feb42bb74ea5bb72988602e70f704065fcdca
gunterbr <josiel_jwk@hotmail.com>
2022-08-02 08:50:06 -0400

    commit

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ app.post('/newuser', (req, res) => {
 
   const { user, username, password } = req.body
 
-  const check = `SELECT count(username) FROM login WHERE username = ?`
+  const check = `SELECT COUNT(username) AS equalUser FROM login WHERE username = ?`
   connection.query(check, username, (error, count) => {
     if (error) throw error
     
-      res.send(count.data.length)
+      res.send(count[0].equalUser)
       
   })
 
