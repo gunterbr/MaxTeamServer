abbefb36f68fe75a6d21edc09ab3af8dacc92eb1
gunterbr <josiel_jwk@hotmail.com>
2022-08-03 09:37:36 -0400

    commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: '10mb' }))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header(
-      'Access-Control-Allow-Header',
+      'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
 
