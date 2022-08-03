095cbb3f7fe00fd2c2112e6790dd5a567abcd144
gunterbr <josiel_jwk@hotmail.com>
2022-08-03 14:29:28 -0400

    commit

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/login', (req, res) => {
 	connection.query(query, [username, password], (err, result) => {
 		if (err) throw err
 		if (result.length > 0) {
-			res.send(result.length)
+			res.send(JSON.stringify(result.length))
 		} else {
 			res.send('Usuário ou senha incorretos!')
 		}
