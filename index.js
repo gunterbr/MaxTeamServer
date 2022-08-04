69d23add8260e48b2e0b13ee5647f3fc5bad0169
gunterbr <josiel_jwk@hotmail.com>
2022-08-04 13:01:49 -0400

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))//old false
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({ limit: '10mb' }))
 
 app.use((req, res, next) => {
