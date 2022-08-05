d1c5b31fb585da846524fbb3402285ae79a8245e
gunterbr <josiel_jwk@hotmail.com>
2022-08-05 00:54:41 -0400

    commit

--- index.js
+++ index.js
@@ -114,8 +114,7 @@ app.post('/inscricao', (req, res) => {
 })
 
 app.post("/upload", upload.array('files', 2), (req, res) => {
-  console.log(req.body, req.files)
-  res.send('ok')
+  res.send(req.body, req.files)
 })
 
 //Confirmar Inscrição
