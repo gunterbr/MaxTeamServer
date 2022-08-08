1edd2ba013fe114229957757d88c3fe7a3682222
gunterbr <josiel_jwk@hotmail.com>
2022-08-08 16:24:51 -0400

    commit aws

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const multerS3 = require('multer-s3')
 
 const s3 = new S3Client({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
 })
 
 const app = express()
